e78e3fe5db56cef5da733f5df53ef8e2666532f7
Palesa Mapeka <palesamapeka97@gmail.com>
2022-09-29 12:58:50 +0200

    updated nav bar to work properly

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+});
+
+navbarLinks.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+});
