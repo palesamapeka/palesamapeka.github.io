78c5cd29c1116e4c750817d1fe10169be3fd744e
Palesa <palesa.mapeka@umuzi.org>
2024-02-02 23:36:56 +0200

    added a scroll animation

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const hiddenElements = document.querySelectorAll(".hidden");
+const observer = new IntersectionObserver((entries) =>{
+  entries.forEach((entry) =>{
+    if(entry.isIntersecting){
+      entry.target.classList.add("show")
+    }else{
+      entry.target.classList.remove("show")
+    }
+  })
+})
+
+hiddenElements.forEach((el) => observer.observe(el));
