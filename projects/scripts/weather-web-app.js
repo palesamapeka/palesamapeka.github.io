a0d5ad8988e7155cab968a56f0c32cb35fce6721
Palesa <palesa.mapeka@umuzi.org>
2024-06-14 22:34:12 +0200

    added search capability

--- projects/scripts/weather-web-app.js
+++ projects/scripts/weather-web-app.js
@@ -1,21 +1,26 @@
 const apiKey = "48a921e65e456f3fc7892e6c6ac4aa4f";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=welkom";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function getWeatherInfo() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+let searchBox = document.querySelector(".search-bar input");
+let searchButton = document.querySelector(".search-bar button");
+
+async function getWeatherInfo(cityName) {
+  const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
   let data = await response.json();
   console.log(data.main);
 
   let city = document.querySelector(".city-name");
   let temperature = document.querySelector(".temperature");
   let humidity = document.querySelector(".humidity");
-  let wind = document.querySelector(".wind")
+  let wind = document.querySelector(".wind");
 
   city.innerHTML = data.name;
-  temperature.innerHTML = (data.main.temp).toFixed(0) + "°C";
+  temperature.innerHTML = Math.round(data.main.temp) + "°C";
   humidity.innerHTML = data.main.humidity + "%";
   wind.innerHTML = data.wind.speed + " km/h";
-
 }
 
-getWeatherInfo();
+searchButton.addEventListener("click", () => {
+  getWeatherInfo(searchBox.value);
+});
