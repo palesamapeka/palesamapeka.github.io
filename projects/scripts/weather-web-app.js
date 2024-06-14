62d8debab104c6490268485a5615429d984c932b
Palesa <palesa.mapeka@umuzi.org>
2024-06-14 23:51:35 +0200

    updated weather app

--- projects/scripts/weather-web-app.js
+++ projects/scripts/weather-web-app.js
@@ -4,21 +4,40 @@ const apiUrl =
 
 let searchBox = document.querySelector(".search-bar input");
 let searchButton = document.querySelector(".search-bar button");
+let suggestionUrl = "https://api.openweathermap.org/data/2.5/find?";
 
 async function getWeatherInfo(cityName) {
   const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
   let data = await response.json();
-  console.log(data.main);
+  console.log(data);
 
   let city = document.querySelector(".city-name");
   let temperature = document.querySelector(".temperature");
   let humidity = document.querySelector(".humidity");
   let wind = document.querySelector(".wind");
+  let weatherIcon = document.querySelector(".weather-icon");
+  let weatherInfo = document.querySelector(".weather");
 
   city.innerHTML = data.name;
   temperature.innerHTML = Math.round(data.main.temp) + "°C";
   humidity.innerHTML = data.main.humidity + "%";
   wind.innerHTML = data.wind.speed + " km/h";
+
+  if (data.weather[0].main === "Clear") {
+    weatherIcon.src = "/assets/clear.png";
+  } else if (data.weather[0].main === "Clouds") {
+    weatherIcon.src = "/assets/clouds.png";
+  } else if (data.weather[0].main === "Drizzle") {
+    weatherIcon.src = "/assets/drizzle.png";
+  } else if (data.weather[0].main === "Mist") {
+    weatherIcon.src = "/assets/mist.png";
+  } else if (data.weather[0].main === "Rain") {
+    weatherIcon.src = "/assets/rain.png";
+  } else if (data.weather[0].main === "Snow") {
+    weatherIcon.src = "/assets/snow.png";
+  }
+
+  weatherInfo.style.display = "block";
 }
 
 searchButton.addEventListener("click", () => {
