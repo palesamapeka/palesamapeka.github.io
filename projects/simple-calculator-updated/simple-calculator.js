39d0465f5198673a2b2e84a827f14b96447c4b7a
Palesa <palesa.mapeka@umuzi.org>
2024-06-18 16:29:28 +0200

    updated clac

--- projects/simple-calculator-updated/simple-calculator.js
+++ projects/simple-calculator-updated/simple-calculator.js
@@ -1,9 +1,24 @@
 let buttons = document.querySelectorAll('input[type="button"]')
-let screen = document.getElementsByName("display")
+let screen = document.getElementsByName("display")[0];
+
+console.log(screen)
 
 buttons.forEach(function(selectedButton){
     selectedButton.addEventListener("click", function(event){
         let buttonValue = this.value;
-        screen.value = buttonValue;
+
+        if(buttonValue === "DE"){
+            screen.value = screen.value.slice(0, -1);
+        }else if(buttonValue === "AC"){
+            screen.value = "";
+        }else if(buttonValue === "="){
+            try{
+                screen.value = eval(screen.value)
+            }catch(error){
+                screen.value += error.message
+            }
+        }else{
+            screen.value += buttonValue;
+        }
     })
 })
