baf230effb230c20309e30e192a301d19debc9aa
Palesa <palesa.mapeka@umuzi.org>
2024-06-18 15:40:06 +0200

    updated site

--- projects/to-do-list/to-do-list-app.js
+++ projects/to-do-list/to-do-list-app.js
@@ -1,4 +1,5 @@
 const inputBox = document.getElementById("inputBox");
+let addTaskButton = document.querySelector(".row button");
 const listContainer = document.getElementById("list-container");
 const modal = document.getElementById("myModal");
 
@@ -7,31 +8,44 @@ function addTask() {
     modal.style.display = "block"; // Display the modal
   } else {
     //create the element
-    let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
-
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
+    createTasksElement();
   }
   //clear input box
   inputBox.value = "";
   saveLocalData();
 }
 
-function saveLocalData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+function createTasksElement() {
+  let li = document.createElement("li");
+  li.innerHTML = inputBox.value;
+  listContainer.appendChild(li);
+
+  let span = document.createElement("span");
+  span.innerHTML = "\u00d7";
+  li.appendChild(span);
 }
 
-function getLocalData(){
-    listContainer.innerHTML = localStorage.getItem("data")
+function saveLocalData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function getLocalData() {
+  listContainer.innerHTML = localStorage.getItem("data");
 }
 getLocalData();
 
 function closeModal() {
   modal.style.display = "none"; // Hide the modal
 }
+
+addTaskButton.addEventListener("click", createTasksElement);
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+      createTasksElement()
+      inputBox.value = "";
+  }
+});
 
 listContainer.addEventListener("click", function (event) {
   if (event.target.tagName === "LI") {
