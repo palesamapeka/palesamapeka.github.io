1b9707abb917e163b8527ea6afb850e98217695c
Palesa <palesa.mapeka@umuzi.org>
2024-06-20 22:59:07 +0200

    added the clock

--- projects/calendar-web-app/calendar.js
+++ projects/calendar-web-app/calendar.js
@@ -2,13 +2,21 @@ const day = document.getElementById("day");
 const weekDay = document.getElementById("week-day");
 const month = document.getElementById("month");
 const year = document.getElementById("year");
+const hero = document.getElementById("hero")
 
-let date = new Date();
+const hour = document.getElementById("hour");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
 
-day.innerHTML = date.getDate();
-weekDay.innerHTML = getDayName();
-month.innerHTML = getMonthName();
-year.innerHTML = date.getFullYear();
+function updateDate() {
+  let date = new Date();
+  day.innerHTML = date.getDate();
+  weekDay.innerHTML = getDayName();
+  month.innerHTML = getMonthName();
+  year.innerHTML = date.getFullYear();
+}
+
+updateDate();
 
 function getDayName() {
   let weekday = [
@@ -39,3 +47,18 @@ function getMonthName() {
   ];
   return months[new Date().getMonth()];
 }
+
+function updateTime() {
+  let date = new Date();
+  hour.innerHTML = String(date.getHours()).padStart(2, "0");
+  minutes.innerHTML = String(date.getMinutes()).padStart(2, "0");
+  seconds.innerHTML = String(date.getSeconds()).padStart(2, "0");
+}
+
+setInterval(updateTime, 1000);
+updateTime();
+
+/**
+ * Use the setInterval to dynamically update the UI with something cool, I don't know what yet.
+ */
+
