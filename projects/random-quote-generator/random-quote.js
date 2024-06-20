3cebd91267899db69ca45cf3df1f28f9f86eac07
Palesa <palesa.mapeka@umuzi.org>
2024-06-20 16:58:15 +0200

    successfully added webpack

--- projects/random-quote-generator/random-quote.js
+++ projects/random-quote-generator/random-quote.js
@@ -1,4 +1,4 @@
-const {textToSpeechOnHover} = require("../global-functions/text-to-speech");X
+const {textToSpeechOnHover} = require("../global-functions/text-to-speech");
 const apiUrl = "https://api.quotable.io/random";
 const quote = document.getElementById("quote");
 const newQuoteBtn = document.getElementById("new-quote");
@@ -36,3 +36,29 @@ textToSpeechOnHover(quote);
 textToSpeechOnHover(author);
 textToSpeechOnHover(newQuoteBtn);
 textToSpeechOnHover(tweetBtn);
+
+/**
+ * Installing Webpack:
+ * 
+ * 1. npm init
+ * 2. npm i webpack webpack-cli --save-dev
+ * 
+ * Sometimes the webpack config file is not created automatically, here's what you do
+ * If you don't have a webpack configuration file, you can specify the entry and output 
+ * files directly in the command line:
+ * `npx webpack [entry point of your code (relative path)] -o ./dist/bundle.js`
+ * 
+ * 3. Delete the dist folder and create one manually
+ * 4. Create a webpack configuration file (webpack.config.js)
+ *    const path = require('path');
+
+      module.exports = {
+        entry: './src/index.js', // Entry point of your application
+        output: {
+        path: path.resolve(__dirname, 'dist'), // Output directory
+       filename: 'bundle.js', // Output filename
+  },
+}; 5.  Include the bundled file in your HTML <scripts src="dist/bundle.js"></scripts>
+   5. run `npm run build` or `npx webpack --watch` to automatically bundle your code
+      after making changes
+ */
