d9b04321f887295ac7dbffe06118c79cdb9c8791
Palesa <palesa.mapeka@umuzi.org>
2024-08-06 19:17:51 +0200

    updates

--- projects/random-quote-generator/webpack.config.js
+++ projects/random-quote-generator/webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-  entry: './random-quote.js',
+  mode: "development",
+  entry: "./random-quote.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
 };
