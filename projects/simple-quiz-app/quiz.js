b15a26e5fc7695a44075829067f568433990b426
Palesa <palesa.mapeka@umuzi.org>
2024-08-06 21:22:49 +0200

    completed quiz game

--- projects/simple-quiz-app/quiz.js
+++ projects/simple-quiz-app/quiz.js
@@ -3,9 +3,9 @@
 const { questionsAndAnswersArr } = require("./helper-array");
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextBtn = document.getElementById("next-btn");
-const scoreElement = document.getElementById("score")
+const scoreElement = document.getElementById("score");
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -18,6 +18,7 @@ function startQuiz() {
 }
 
 function showQuestion() {
+  resetState();
   let currentQuestion = questionsAndAnswersArr[currentQuestionIndex];
   let questionNum = currentQuestionIndex + 1;
   questionElement.innerHTML = questionNum + ". " + currentQuestion.question;
@@ -26,8 +27,68 @@ function showQuestion() {
     const button = document.createElement("button");
     button.innerHTML = answer.text;
     button.classList.add("btn");
-    answerButton.appendChild(button);
+    answerButtons.appendChild(button);
+
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
   });
+}
+
+function resetState() {
+  nextBtn.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+    scoreElement.innerHTML += "🔥";
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+    nextBtn.style.display = "block";
+  });
+}
+
+nextBtn.addEventListener("click", nextQuestion);
+
+function nextQuestion() {
+  if (currentQuestionIndex < questionsAndAnswersArr.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questionsAndAnswersArr.length) {
+    showQuestion();
+  } else {
+    showScore();
+    scoreElement.innerHTML = "";
+  }
+}
+
+function showScore() {
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questionsAndAnswersArr.length}`;
+  nextBtn.innerHTML = "Play Again";
+  nextBtn.style.display = "block";
 }
 
 startQuiz();
